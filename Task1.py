65efa39b103e3badcf6f1cbafb8d7a3572a19792
ishaantiwari03 <55748772+ishaantiwari03@users.noreply.github.com>
2019-12-06 20:31:56 +0530

    NLP TASKS

---
+++ Task1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+text = "shakespeare gave the world best of theatrics"
+
+
+# In[3]:
+
+
+print (text)
+
+
+# In[4]:
+
+
+text.split(" ")
+
+
+# In[5]:
+
+
+text1="William Shakespeare, Shakespeare also spelled Shakspere, byname Bard of Avon or Swan of Avon, (baptized April 26, 1564, Stratford-upon-Avon, Warwickshire, England—died April 23, 1616, Stratford-upon-Avon), English poet, dramatist, and actor often called the English national poet and considered by many to be the greatest dramatist of all time."
+
+
+# In[8]:
+
+
+print(text1)
+
+
+# In[16]:
+
+
+text = 'William Shakespeare
+Shakespeare also spelled Shakspere, 
+byname Bard of Avon or Swan of Avon, (baptized April 26, 1564, Stratford-upon-Avon, Warwickshire, England—died April 23, 1616, Stratford-upon-Avon), English poet, dramatist,
+and actor often called the English national poet 
+and considered by many to be the greatest dramatist of all time.'
+
+
+# In[10]:
+
+
+print(text)
+
+
+# In[13]:
+
+
+text='''William Shakespeare, Shakespeare also spelled Shakspere, 
+byname Bard of Avon or Swan of Avon, (baptized April 26, 1564, Stratford-upon-Avon,
+Warwickshire, England—died April 23, 1616,
+Stratford-upon-Avon),
+English poet,
+dramatist,
+and actor often called the English national poet and considered by many to be the greatest dramatist of all time.'''
+
+
+# In[14]:
+
+
+print (text)
+
+
+# In[18]:
+
+
+text = """﻿हो। गए,
+उनका एक समय में बड़ा नाम था। पूरे देश में तालाब बनते थे ब
+नाने वाले भी पूरे देश में थे। कहीं यह विद्या जाति के विद्यालय | स
+िखाई जाती थी तो कहीं यह जात से हट कर एक विशेष पांत भी ज
+ाती थी। बनाने वाले लोग कहीं एक जगह बसे मिलते थे तो कहीं -घूम कर इस काम को करते थे। I 국 घम गजधर एक सुन्दर शब्द है, तालाब बनाने वालों को आदर के साथ याद करने के लिए। राजस्थान के कुछ भागों में यह शब्द आज भी बाकी है। गजधर यानी जो गज को धारण करता है। और गज वही जो नापने के काम आता है। लेकिन फिर भी समाज ने इन्हें तीन हाथ की लोहे की छड़ लेकर घूमने वाला मिस्त्री नहीं माना। गजधर जो समाज को गहराई को नाप ले - उसे ऐसा दर्जा दिया गया है। गजधर वास्तुकार थे। गांव-समाज हो या नगर-समाज - उसके नव निर्माण की, रख-रखाव की ज़िम्मेदारी गजधर निभाते थे। नगर नियोजन से लेकर छोटे से छोटे निर्माण के काम गजधर के कधों पर टिके थे। वे योजना बनाते थे, कुल काम की लागत निकालते थे, काम में लगने वाली सारी सामग्री जुटाते थे और इस सबके बदले वे अपने जजमान से ऐसा कुछ नहीं मांग बैठते थे, जो वे दे न पाएं। लोग भी ऐसे थे कि उनसे जो कुछ बनता, वे गजधर को भेंट कर देते। काम पूरा होने पर पारिश्रमिक के अलावा गजधर को सम्मान ' भी मिलता था।"""
+
+
+# In[19]:
+
+
+print(text)
+
+
+# In[ ]:
+
+
+
+
